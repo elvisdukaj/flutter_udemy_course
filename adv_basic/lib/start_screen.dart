75362dea5d2667a27c0b531b4e733d2c9952ff09
Elvis Dukaj <elvis.dukaj@dynatrace.com>
2025-08-25 11:53:40 +0200

    Adding start screen widget

---
+++ adv_basic/lib/start_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+            color: Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 40),
+          const Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              color: Color.fromARGB(150, 255, 255, 255),
+              fontSize: 28,
+            ),
+          ),
+          const SizedBox(height: 40),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text("Start Quick"),
+          ),
+        ],
+      ),
+    );
+  }
+}
